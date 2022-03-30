9e54cadd1e483a86095ac90968665ce1d05026c2
emily <emily@bacheconsulting.com>
2022-03-30 11:14:40 +0200

    make it more pythonic

--- python/tests/test_turn_ticket.py
+++ python/tests/test_turn_ticket.py
@@ -3,19 +3,19 @@ import pytest
 from turn_ticket import TicketDispenser, TurnTicket
 
 def test_dispenser():
-    dispenser = TicketDispenser()
+    dispenser = TicketDispenser("front")
     ticket = dispenser.next_turn_ticket()
-    assert ticket == TurnTicket(0)
+    assert ticket == TurnTicket(0, "front")
 
 
 def test_two_dispensers():
-    dispenser1 = TicketDispenser()
-    dispenser2 = TicketDispenser()
+    dispenser1 = TicketDispenser("front")
+    dispenser2 = TicketDispenser("side")
     ticket1 = dispenser1.next_turn_ticket()
     ticket2 = dispenser2.next_turn_ticket()
     ticket3 = dispenser1.next_turn_ticket()
 
     # TODO: make this work
-    assert ticket1 == TurnTicket(0)
-    assert ticket2 == TurnTicket(1)
-    assert ticket3 == TurnTicket(2)
+    assert ticket1 == TurnTicket(0, "front")
+    assert ticket2 == TurnTicket(1, "side")
+    assert ticket3 == TurnTicket(2, "front")
--- python/turn_ticket.py
+++ python/turn_ticket.py
@@ -3,24 +3,30 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class TurnTicket:
+    # which turn number you have in the queue
     turnNumber: int
+    # which dispenser gave the ticket
+    dispenser_name: str
 
 
-_turnNumber = -1
+_turnNumber = 0
 
 
 def next_turn_number():
     global _turnNumber
     while True:
-        _turnNumber += 1
         yield _turnNumber
+        _turnNumber += 1
 
 
 _ticket_provider = next_turn_number()
 
 
 class TicketDispenser:
+    def __init__(self, name):
+        self.name = name
+
     def next_turn_ticket(self):
         next_turn_number = _ticket_provider.__next__()
-        ticket = TurnTicket(next_turn_number)
+        ticket = TurnTicket(next_turn_number, self.name)
         return ticket
