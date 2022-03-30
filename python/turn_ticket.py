23edef8868bcdf3a4aa461a2076cc70633009b17
emily <emily@bacheconsulting.com>
2022-03-30 11:08:35 +0200

    solution attempt

--- python/turn_ticket.py
+++ python/turn_ticket.py
@@ -6,17 +6,20 @@ class TurnTicket:
     turnNumber: int
 
 
-class TurnNumberSequence:
-    _turnNumber = -1
+_turnNumber = -1
 
-    @staticmethod
-    def next_turn_number():
-        TurnNumberSequence._turnNumber += 1
-        return TurnNumberSequence._turnNumber
+
+def next_turn_number():
+    global _turnNumber
+    while True:
+        _turnNumber += 1
+        yield _turnNumber
 
 
 class TicketDispenser:
+    def __init__(self, ticket_provider=None):
+        self.ticket_provider = ticket_provider or next_turn_number()
     def next_turn_ticket(self):
-        next_turn_number = TurnNumberSequence.next_turn_number()
+        next_turn_number = self.ticket_provider.__next__()
         ticket = TurnTicket(next_turn_number)
         return ticket
