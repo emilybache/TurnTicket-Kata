97d74aa50eae9f7c40f94d5bfaf67a13597ebea6
emily <emily@bacheconsulting.com>
2022-06-28 15:27:57 +0200

    initial csharp version

---
+++ csharp/TurnTicket/TicketDispenser.cs
@@ -0,0 +1,12 @@
+namespace Turns;
+
+public class TicketDispenser
+{
+    public TurnTicket GetTurnTicket()
+    {
+        int newTurnNumber = TurnNumberSequence.GetNextTurnNumber();
+        TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
+
+        return newTurnTicket;
+    }
+}
